d3966202fa1851b114ef05bc645a1cb6d9e98c28
Felipe Barros Muniz <felipemuniz@alu.ufc.br>
2021-12-13 23:19:51 -0300

    Começo do feed modal

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PhotoComments from './PhotoComments';
 import styles from './PhotoContent.module.css';
 
 const PhotoContent = ({ data }) => {
-  return <div></div>;
+  const { photo, comments } = data;
+  return (
+    <div className={styles.photo}>
+      <div className={styles.img}>
+        <img src={photo.src} alt={photo.title} />
+      </div>
+      <div className={styles.details}>
+        <div>
+          <p>
+            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            <span className={styles.visualizacoes}>{photo.acessos}</span>
+          </p>
+          <h1 className="title">
+            <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.pseo} kg</li>
+            <li>
+              {photo.idade > 1 ? photo.idade + ' anos' : photo.idade + ' ano'}{' '}
+            </li>
+            <li>{photo.pseo}</li>
+          </ul>
+        </div>
+      </div>
+      <PhotoComments id={photo.id} comments={comments} />
+    </div>
+  );
 };
 
 export default PhotoContent;
