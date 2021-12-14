251b4d55acb55777eb9a0037556db77eb33a1c21
Felipe Barros Muniz <felipemuniz@alu.ufc.br>
2021-12-14 00:42:38 -0300

    Estilização do modal de fotos

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -12,7 +12,7 @@ const PhotoContent = ({ data }) => {
       </div>
       <div className={styles.details}>
         <div>
-          <p>
+          <p className={styles.author}>
             <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
             <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
@@ -20,11 +20,10 @@ const PhotoContent = ({ data }) => {
             <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
           </h1>
           <ul className={styles.attributes}>
-            <li>{photo.pseo} kg</li>
+            <li>{photo.peso} kg</li>
             <li>
-              {photo.idade > 1 ? photo.idade + ' anos' : photo.idade + ' ano'}{' '}
+              {photo.idade > 1 ? photo.idade + ' anos' : photo.idade + ' ano'}
             </li>
-            <li>{photo.pseo}</li>
           </ul>
         </div>
       </div>
