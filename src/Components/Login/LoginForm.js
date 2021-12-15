ec585a594fd9495deed144e93254d3ef61de5c73
Felipe Barros Muniz <felipemuniz@alu.ufc.br>
2021-12-14 21:13:01 -0300

    Sistema de recuperação de senha e cabeçalhos

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -7,6 +7,7 @@ import Input from '../Forms/Input';
 import Error from '../Helper/Error';
 import styles from './LoginForm.module.css';
 import stylesBtn from '../Forms/Button.module.css';
+import Head from '../Helper/Head';
 
 const LoginForm = () => {
   const username = useForm();
@@ -23,7 +24,8 @@ const LoginForm = () => {
   }
 
   return (
-    <section className="animeLeft" /*style={{ padding: '2rem 25%' }}*/>
+    <section className="animeLeft">
+      <Head title="Login" />
       <h1 className="title">Login</h1>
       <form className={styles.form} onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
