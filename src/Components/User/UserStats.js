0bdbf8f6216305c8679d2faf7bf684f906f3b28c
Felipe Barros Muniz <felipemuniz@alu.ufc.br>
2021-12-14 23:19:24 -0300

    Adição da bibliotece responsável pelos gráficos

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import Head from '../Helper/Head';
+import useFetch from '../../Hooks/useFetch';
+import react from 'react';
+import { STATS_GET } from '../../api';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+import UserStatsGraphics from './UserStatsGraphics';
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      Estatísticas
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  react.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) <Loading />;
+  if (error) <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphics data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
